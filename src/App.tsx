df79d095ed33a2e42e04cdd4260dfac48eec7189
Yash Gediya <yash.gediya@bytestechnolab.in>
2022-06-10 14:33:44 +0530

    changes in UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import Routing from "./router/Routing";
 
 function App() {
--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,55 +1,53 @@
-import React, { useEffect, useState } from 'react'
-import { Table } from 'react-bootstrap'
+import React, { useEffect, useState } from "react";
+import { Table } from "react-bootstrap";
 
 const Users = () => {
+  const [data, setData]: any = useState();
+  const [mode, setMode] = useState("online");
 
-    const [data, setData]: any = useState()
-    const [mode, setMode] = useState("online")
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response: any) => {
+        response.json().then((result: any) => {
+          setData(result);
+          localStorage.setItem("users", JSON.stringify(result));
+        });
+      })
+      .catch((error) => {
+        setMode("offline");
+        let collection: any = localStorage.getItem("users");
+        setData(JSON.parse(collection));
+      });
+  }, []);
 
-    useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/users")
-            .then((response: any) => {
-                response.json().then((result: any) => {
-                    setData(result)
-                    localStorage.setItem("users", JSON.stringify(result))
-                })
-            })
-            .catch((error) => {
-                setMode("offline")
-                let collection: any = localStorage.getItem("users");
-                setData(JSON.parse(collection));
-            }
-            )
-    }, [])
+  return (
+    <div>
+      <div>
+        {mode === "offline" ? <div>You are in offline Mode</div> : null}
+      </div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>User Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data &&
+            data.map((item: any, index: any) => (
+              <tr key={index}>
+                <td>{item?.id}</td>
+                <td>{item?.name}</td>
+                <td>{item?.username}</td>
+                <td>{item?.email}</td>
+              </tr>
+            ))}
+        </tbody>
+      </Table>
+    </div>
+  );
+};
 
-    return (
-        <div>
-            <div>
-                {mode === "offline" ?
-                    <div>You are in offline Mode</div> : null}
-            </div>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>Name</th>
-                        <th>User Name</th>
-                        <th>Email</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data && data.map((item: any, index: any) => (
-                        <tr key={index}>
-                            <td>{item?.id}</td>
-                            <td>{item?.name}</td>
-                            <td>{item?.username}</td>
-                            <td>{item?.email}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </Table>
-        </div>
-    )
-}
-
-export default Users
+export default Users;
