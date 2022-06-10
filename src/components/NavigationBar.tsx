a298f498483bb81cfb544d25693cb1bbe1a300be
Yash Gediya <yash.gediya@bytestechnolab.in>
2022-06-10 16:12:22 +0530

    changes

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,17 +1,14 @@
 import React from "react";
-import { Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const NavigationBar = () => {
   return (
-    <div>
-      <Navbar bg="primary" variant="dark">
-        <Nav>
-          <Link to="/">Home</Link>
-          <Link to="users">Users</Link>
-          <Link to="about">About</Link>
-        </Nav>
-      </Navbar>
+    <div className="nav-bar">
+      <nav className="nav-link">
+        <Link to="/">Home</Link>
+        <Link to="users">Users</Link>
+        <Link to="about">About</Link>
+      </nav>
     </div>
   );
 };
